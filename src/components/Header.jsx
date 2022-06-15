dbb6091825faa726cfe28fd8f9a6e33db21a6c2c
Jackson Leb <jacksonleb@Jacksons-MBP.lan>
2022-06-15 16:46:34 -0400

    fixed css animation on safari 15+

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,17 @@ import Resume from '../assets/JacksonLeb2024.pdf'
 function Header() {
   return (
     <div className='header'>
-        <h1 className='headerMain text-good-blue font-bold text-5xl ml-2 mt-2'>
+        <h1 className='headerMain text-good-blue font-bold text-5xl ml-4 mt-4'>
           <a href='/' className=''>
             Jackson Leb
           </a>
         </h1>
-        <h3 className='text-good-blue font-semibold text-xl ml-2'>
+        <h3 className='text-good-blue font-semibold text-xl ml-4'>
           <a href='/' className='headerSecondary'>
             Developer / Leader / Student
           </a>
         </h3>
-        <div className='hotButtons text-good-blue grid grid-cols-2 text-2xl inline w-1/4 pointer-events-auto'>
+        <div className='hotButtons text-good-blue grid grid-cols-2 text-2xl inline w-1/2 pointer-events-auto'>
           <a href='https://www.linkedin.com/in/jackson-leb/'>
             <FaLinkedin />
           </a>
